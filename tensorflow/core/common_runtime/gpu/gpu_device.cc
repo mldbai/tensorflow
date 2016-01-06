f811abbbfc4e44f34eee007ade50f5c72cd43986
Jeremy Barnes <jeremy@datacratic.com>
2016-01-05 21:41:47 -0500

    Allow use on compute model 3.0, ie for AWS GPU instances (MLDB-1203)

--- tensorflow/core/common_runtime/gpu/gpu_device.cc
+++ tensorflow/core/common_runtime/gpu/gpu_device.cc
@@ -637,7 +637,7 @@ struct CudaVersion {
 // "configure" uses the specific name to substitute the following string.
 // If you change it, make sure you modify "configure" as well.
 std::vector<CudaVersion> supported_cuda_compute_capabilities = {
-    CudaVersion("3.5"), CudaVersion("5.2")};
+    CudaVersion("3.0"), CudaVersion("3.5"), CudaVersion("5.2")};
 
 }  // namespace
 
